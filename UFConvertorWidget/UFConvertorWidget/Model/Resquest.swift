e59a829a6cf527db056baab5c212b0360c712f94
ClaireRimel <sivadiercl@gmail.com>
2020-05-15 14:28:53 +0200

    New: RequestResponse to get the information from a JSON Response

---
+++ UFConvertorWidget/UFConvertorWidget/Model/Resquest.swift
@@ -0,0 +1,23 @@
+//
+//  Resquest.swift
+//  UFConvertorWidget
+//
+//  Created by Claire on 15/05/2020.
+//  Copyright © 2020 Claire Sivadier. All rights reserved.
+//
+
+import Foundation
+
+struct RequestResponse: Codable {
+    let serie: [Serie]
+}
+
+struct Serie: Codable {
+    let date: Date
+    let value: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case date = "fecha"
+        case value = "valor"
+    }
+}
