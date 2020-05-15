f2a91624fb07aeff5f486d5e1e57a10dc58e551e
ClaireRimel <sivadiercl@gmail.com>
2020-05-15 21:15:40 +0200

    New: Implemented the conversion's logic part + Handling error cases

---
+++ UFConvertorWidget/UFConvertorWidget/Model/CurrencyConverterError.swift
@@ -0,0 +1,32 @@
+//
+//  CurrencyConverterError.swift
+//  UFConvertorWidget
+//
+//  Created by Claire on 15/05/2020.
+//  Copyright © 2020 Claire Sivadier. All rights reserved.
+//
+
+import Foundation
+
+//typed errors
+enum CurrencyConverterError: Error, Equatable {
+    case requestError(NSError)
+    case invalidResponseFormat
+    case usdRateNotFound
+    case invalidInput
+}
+
+extension CurrencyConverterError {
+    var message: String{
+        switch self {
+        case let .requestError(error):
+            return error.localizedDescription
+        case .invalidResponseFormat:
+            return "Le format de réponse du serveur est invalide "
+        case .usdRateNotFound:
+            return "La devise USD n'est pas disponible"
+        case .invalidInput:
+            return "Entrez un montant valide"
+        }
+    }
+}
--- UFConvertorWidget/UFConvertorWidget/Model/RequestModel.swift
+++ UFConvertorWidget/UFConvertorWidget/Model/RequestModel.swift
@@ -10,24 +10,65 @@ import Foundation
 
 final class RequestModel {
     
+    enum RequestDateRequired: Equatable {
+        case required
+        case notRequired
+    }
+    
+    struct LatestRateAndDate {
+        var clpRate: Double
+        var requestDate: String
+    }
+    
+    var latestRateAndDate: LatestRateAndDate?
+    
     var series: [Serie] = []
     
-    func openSessionRequest(then: @escaping (Result<RequestResponse, Error>) -> Void) {
+    func convert(from: String, then: @escaping (Result<Double, CurrencyConverterError>) -> Void) {
+        
+        // Verify if the String can be converter to a Double, if not, a error message will be displayed.
+        guard let value = convertToDouble(from: from)
+            else {
+                then(.failure(.invalidInput))
+                return
+        }
+        
+        // Verify if the latest request was made the same day, to do the conversion with the latest known rate received, otherwise, to process to a new request
+        if let latestRateAndDate = latestRateAndDate, wasRequestMadeToday(requestDate: latestRateAndDate.requestDate) {
+            
+            let clpRate = value * latestRateAndDate.clpRate
+            DispatchQueue.main.async {
+                then(.success(clpRate))
+            }
+        } else {
+            request(required:.required, then: { (result) in
+                switch result {
+                case .success:
+                    self.convert(from: from, then: then)
+                case let .failure(error):
+                    //TODO: HANDLE ERROR
+                    break
+                }
+            })
+        }
+    }
+    
+    func request(required: RequestDateRequired,  then: @escaping (Result<RequestResponse, Error>) -> Void) {
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd-MM-yyyy"
         let dateToday = dateFormatter.string(from: Date())
         
-//        let path = "/api/uf/\(dateToday)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
-        
         var components = URLComponents()
         components.scheme = "https"
         components.host = "mindicador.cl"
-        components.path = "/api/uf/\(dateToday)"
-
-
         
-        
+        if required == .required {
+            components.path = "/api/uf/\(dateToday)"
+        } else {
+            components.path = "/api/uf"
+        }
+
         //Gets URL object based on given components
         let url = components.url!
         print(url)
@@ -65,6 +106,8 @@ final class RequestModel {
                             return
                 }
                 self.series = decodedResponse.serie
+                self.latestRateAndDate = LatestRateAndDate(clpRate: decodedResponse.serie[0].value , requestDate:  decodedResponse.serie[0].date)
+
                 DispatchQueue.main.async {
                     then(.success(decodedResponse))
                     print(decodedResponse)
@@ -88,4 +131,26 @@ final class RequestModel {
         })
         task.resume()
     }
+    
+    // Use to format today's date and compare it with a given date
+      func wasRequestMadeToday(requestDate: String) -> Bool {
+//        "2020-05-15T04:00:00.000Z"
+        
+        
+//          let date = Date()
+          let format = DateFormatter()
+          format.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        
+        if let dateToCompare = format.date(from: requestDate) {
+            if Calendar.current.compare(Date(), to: dateToCompare, toGranularity: .day) == .orderedSame {
+                return true
+            }
+        }
+        return false
+      }
+    
+    // Use to convert a curreny to a Double
+       func convertToDouble(from currency: String)-> Double? {
+           return Double(currency)
+       }
 }
