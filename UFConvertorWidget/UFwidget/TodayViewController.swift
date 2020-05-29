fea4d3b1e02b3dba4e0f07011ea80e20720cfe5e
ClaireRimel <sivadiercl@gmail.com>
2020-05-29 11:52:18 +0200

    Chg: hide graph in compact mode and show it with an animation if the activeDisplayMode is expended.  Change the line chart style

--- UFConvertorWidget/UFwidget/TodayViewController.swift
+++ UFConvertorWidget/UFwidget/TodayViewController.swift
@@ -19,31 +19,31 @@ class TodayViewController: UIViewController, NCWidgetProviding, ChartViewDelegat
     
     let model = RequestModel()
     
-  lazy var lineChartView: LineChartView = {
-       let chartview = LineChartView()
-       chartview.backgroundColor = .clear
-       chartview.rightAxis.enabled = false
-       chartview.leftAxis.enabled = false
-       chartview.legend.enabled = false
-       chartview.xAxis.enabled = false
-       
-       return chartview
-   }()
+    lazy var lineChartView: LineChartView = {
+        let chartview = LineChartView()
+        chartview.backgroundColor = .clear
+        chartview.rightAxis.enabled = false
+        chartview.leftAxis.enabled = false
+        chartview.legend.enabled = false
+        chartview.xAxis.enabled = false
+        
+        return chartview
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded
+        
         // Do any additional setup after loading the view.
         request()
         lineChartView.frame = graphContainerView.frame
         lineChartView.frame.origin.y = 0
         graphContainerView.addSubview(lineChartView)
-       
-
     }
     
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         completionHandler(NCUpdateResult.newData)
+        drawGraph()
     }
     
     func request() {
@@ -52,7 +52,7 @@ class TodayViewController: UIViewController, NCWidgetProviding, ChartViewDelegat
             case let .success(clpValue):
                 let value = ConvertDouble.convertDoubleToCurrency(amount: clpValue, locale: Locale(identifier: "es_CL"))
                 self.priceLabel.text = value
-                self.drawGraph()
+                
                 
                 let diffenceValue = self.model.differenceValue()
                 if diffenceValue < 0 {
@@ -72,29 +72,34 @@ class TodayViewController: UIViewController, NCWidgetProviding, ChartViewDelegat
     }
     
     func drawGraph() {
-           let setChart = SetChart(series: model.series)
-           let set1 = LineChartDataSet(entries: setChart.chartData(), label: "CLP")
-           
-           set1.drawCirclesEnabled = false
-           set1.mode = .cubicBezier
-           set1.lineWidth = 3
-           set1.setColor(.black)
-           set1.drawHorizontalHighlightIndicatorEnabled = false
-           set1.highlightColor = .systemRed
-           set1.highlightLineWidth = 2
-           
-           let data = LineChartData(dataSet: set1)
-           data.setDrawValues(false)
-           lineChartView.data = data
-       }
+        let setChart = SetChart(series: model.series)
+        let set1 = LineChartDataSet(entries: setChart.chartData(), label: "CLP")
+        
+        set1.drawCirclesEnabled = false
+        set1.mode = .linear
+        set1.lineWidth = 3
+        set1.setColor(.black)
+        set1.drawHorizontalHighlightIndicatorEnabled = false
+        set1.highlightColor = .systemRed
+        set1.highlightLineWidth = 2
+        
+        let data = LineChartData(dataSet: set1)
+        data.setDrawValues(false)
+        lineChartView.data = data
+    }
 }
 
 extension TodayViewController {
     func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
         if activeDisplayMode == .compact {
             self.preferredContentSize = maxSize
+            graphContainerView.isHidden = true
+            
         } else if activeDisplayMode == .expanded {
-            self.preferredContentSize = CGSize(width: maxSize.width, height: 150)
+            self.preferredContentSize = CGSize(width: maxSize.width, height: 200)
+            graphContainerView.isHidden = false
+            lineChartView.frame.size.height = 150
+            lineChartView.animate(xAxisDuration: 1.0)
         }
     }
 }
